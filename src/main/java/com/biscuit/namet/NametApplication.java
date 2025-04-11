26c62422f2c3cef8c6bc935aeaeac759c038d6a3
eyvaz.bayramov <eyvaz.bayramov2018@gmail.com>
2025-04-11 22:34:58 +0400

    initial commit

---
+++ src/main/java/com/biscuit/namet/NametApplication.java
@@ -0,0 +1,16 @@
+package com.biscuit.namet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NametApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                NametApplication.class,
+                args
+        );
+    }
+
+}
---
+++ src/test/java/com/biscuit/namet/NametApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biscuit.namet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NametApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
